7e16bb12e2ec4293cca29cc2fc3dccdbb75dcf03
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-06-08 16:06:51 -0500

    update Task

---
+++ alx-project-0x02/pages/About.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/layout/Header";
+import Link from "next/link";
+import React from "react";
+import { HeaderProps } from "@/interfaces";
+
+const About: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default About;
